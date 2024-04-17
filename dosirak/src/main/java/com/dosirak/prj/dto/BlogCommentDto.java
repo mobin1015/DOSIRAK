d62c73035726c98e6970581eb880d1aa35a4ca16
mobin15 <mobin15@naver.com>
2024-04-17 17:12:10 +0900

    KFIM-47 DTO 롬복추가

--- dosirak/src/main/java/com/dosirak/prj/dto/BlogCommentDto.java
+++ dosirak/src/main/java/com/dosirak/prj/dto/BlogCommentDto.java
@@ -1,7 +1,15 @@
 package com.dosirak.prj.dto;
 
 import java.sql.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class BlogCommentDto {
 	private int commentNo, blogListNo, state, depth, groupNo;
 	private String contents;
--- dosirak/src/main/java/com/dosirak/prj/dto/LikeDto.java
+++ dosirak/src/main/java/com/dosirak/prj/dto/LikeDto.java
@@ -1,5 +1,14 @@
 package com.dosirak.prj.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class LikeDto {
 	private int likeNo, blogListNo;
 	private UserDto user;
