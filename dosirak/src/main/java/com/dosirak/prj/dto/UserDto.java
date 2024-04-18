5f29d96a3008fc68c0e1890e6feeb9adb5a97dee
mobin1015 <123246684+mobin1015@users.noreply.github.com>
2024-04-18 15:51:04 +0900

    Update UserDto.java

--- dosirak/src/main/java/com/dosirak/prj/dto/UserDto.java
+++ dosirak/src/main/java/com/dosirak/prj/dto/UserDto.java
@@ -18,13 +18,7 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 public class UserDto {
-<<<<<<< HEAD
-	private int userNo, signupKind;
-	private String email, pw, name, gender, mobile;
-	private Date pwModifyDt, signupDt;
-=======
   private int userNo, signupKind;
   private String email, pw, name, gender, mobile;
   private Date pwModifyDt, signupDt;
->>>>>>> f7224d81fcaa7dddfd8d5015f5f22f2dc1feacf2
 }
