b3f3fdca5d6d3e021026256511da89a41782e6c6
ted4010 <tlsehddn4010@gmail.com>
2024-04-18 16:01:42 +0900

    no message

--- dosirak/src/main/java/com/dosirak/prj/dto/UserDto.java
+++ dosirak/src/main/java/com/dosirak/prj/dto/UserDto.java
@@ -6,25 +6,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-
-
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Builder
 public class UserDto {
-<<<<<<< HEAD
-	private int userNo, signupKind;
-	private String email, pw, name, gender, mobile;
-	private Date pwModifyDt, signupDt;
-=======
   private int userNo, signupKind;
   private String email, pw, name, gender, mobile;
   private Date pwModifyDt, signupDt;
->>>>>>> f7224d81fcaa7dddfd8d5015f5f22f2dc1feacf2
 }
