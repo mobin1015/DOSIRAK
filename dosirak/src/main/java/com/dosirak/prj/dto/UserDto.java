a12b08f1774a0ac4c1af8fe9d0bd4f9663fce40a
mobin1015 <123246684+mobin1015@users.noreply.github.com>
2024-04-18 15:51:18 +0900

    Update UserDto.java

--- dosirak/src/main/java/com/dosirak/prj/dto/UserDto.java
+++ dosirak/src/main/java/com/dosirak/prj/dto/UserDto.java
@@ -6,13 +6,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-
-
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
