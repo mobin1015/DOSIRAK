db4d1cc62ec8121068e3921eb64795715ad1d2d7
ted4010 <tlsehddn4010@gmail.com>
2024-04-22 14:21:44 +0900

    #	dosirak/src/main/webapp/WEB-INF/views/user/signup.jsp

--- dosirak/src/main/java/com/dosirak/prj/service/UserService.java
+++ dosirak/src/main/java/com/dosirak/prj/service/UserService.java
@@ -1,5 +1,29 @@
 package com.dosirak.prj.service;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.ResponseEntity;
+
+import com.dosirak.prj.dto.UserDto;
+
 public interface UserService {
 
+	//가입
+	void signup(HttpServletRequest request, HttpServletResponse response);
+	void leave(HttpServletRequest request, HttpServletResponse response);
+	ResponseEntity<Map<String, Object>> checkEmail(Map<String, Object> params);
+	ResponseEntity<Map<String, Object>> sendCode(Map<String, Object> params);
+	// 로그인 및 로그아웃
+	String getRedirectURLAfterLogin(HttpServletRequest request);
+	void login(HttpServletRequest request, HttpServletResponse response);
+	void logout(HttpServletRequest request, HttpServletResponse response);
+	// 네이버 로그인
+	String getNaverLoginURL(HttpServletRequest request);
+	String getNaverLoginAccessToken(HttpServletRequest request);
+	UserDto getNaverLoginProfile(String accessToken);
+	boolean hasUser(UserDto user);
+  void naverLogin(HttpServletRequest request, UserDto naverUser);
 }
